ca4ba09daf4e8c538a460ed4a4754babcccf0fb7
Yohei Okubo <okubo.yohei@gmail.com>
2020-01-04 00:58:18 +0900

    setup graphql and mongoose

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ItemsModule } from './items/items.module';
 
+// tmp
+const MONGO_URL = 'mongodb://localhost:27017/react-sample';
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(MONGO_URL),
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+    //
+    ItemsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
