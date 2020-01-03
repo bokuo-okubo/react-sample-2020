7931ec9456782b181071b1316d920c7fb6fdf691
Yohei Okubo <okubo.yohei@gmail.com>
2020-01-04 02:29:42 +0900

    restful endpoints

--- server/src/items/items.controller.ts
+++ server/src/items/items.controller.ts
@@ -1,14 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { ItemsService } from './items.service';
+import { ItemType } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
 
   constructor(private readonly itemsService: ItemsService) {}
   // very tmp
+  count = 0;
+  @Get('create')
+  async create(): Promise<ItemType​​> {
+    return this.itemsService.create({ title: `tomato ${this.count++}`, price: 300, description: 'so sweet.' });
+  }
+
   @Get()
-  async create(): Promise<string> {
-    const item = await this.itemsService.create();
-    return JSON.stringify(item);
+  async all(): Promise<ItemType​​[]> {
+    return this.itemsService.findAll();
   }
 }
--- server/src/items/items.service.ts
+++ server/src/items/items.service.ts
@@ -2,21 +2,33 @@ import { Injectable } from '@nestjs/common';
 
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-// import { ItemType } from './dto/create-item.dto';
+import { ItemType } from './dto/create-item.dto';
 import { Item } from './interfaces/item.interface';
-// import { ItemInput } from './input-items.input';
-
-// very tmp
+import { ItemInput } from './create-items.input';
 
 @Injectable()
 export class ItemsService {
-  private count = 0;
 
   constructor(@InjectModel('Item') private itemModel: Model<Item>) {}
 
-  // very tmp.
-  async create(): Promise<Item> {
-    const createdItem = new this.itemModel({ title: `title: count ${this.count++}`});
+  async create(createItemDto: ItemInput): Promise<ItemType> {
+    const createdItem = new this.itemModel(createItemDto);
     return await createdItem.save();
+  }
+
+  async findAll(): Promise<ItemType[]> {
+    return await this.itemModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<ItemType> {
+    return await this.itemModel.findOne({ _id: id });
+  }
+
+  async delete(id: string): Promise<ItemType> {
+    return await this.itemModel.findByIdAndRemove(id);
+  }
+
+  async update(id: string, item: Item): Promise<ItemType> {
+    return await this.itemModel.findByIdAndUpdate(id, item, { new: true });
   }
 }
